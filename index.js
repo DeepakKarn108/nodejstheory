70d75f9382f017d494205184ee6de132a82021c4
DeepakKarn108 <karndeepak28@gmail.com>
2022-09-14 16:09:44 +0530

    theory of nodejs

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+/*
 Q- what is Nodejs?
 ans- Nodejs is a javascript runtime built on chromes v8 javascript engine.
-    or Nodejs is javascript running on the ServiceWorkerRegistration.
+    or Nodejs is javascript running on the Server.
+    +++++++++++++++++++++++++++++++++++++++++
+    founder:-Ryan dyan in 2009 he took javascript v8 engine and embedded in the c++ called as the node.exe after
+    that it is called as Nodejs.
+   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+   Ques:- when to use Nodejs.
+   Ans  :- when there is input and output bond like like disk,database and any other.
+         :- In data streaming applications.
+          :- real time chat applications.
+          ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+          
+          
+   Ques:-what is Nodejs Repl?
+   Ans:-  the Repl Nodejs is very useful with expermenting with node js and to debug javascript code.
+            the full form is the :=
+            R= Read users input in to javascript data structure,and stores in the memory.
+            e=takes and evalutes the data structure.
+            p=print the result.
+            l=loops the above comm.
+
+
+            
+
+          
